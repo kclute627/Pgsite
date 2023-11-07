f61ebe3fd1c3b1eda9b59778c6d082fe8454c880
hashirkn <hashir.carbonicit@gmail.com>
2023-11-07 22:16:51 +0500

    add header and two more sections only on desktop view

---
+++ js/tailwind.config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        darkblue: "#0A3C5E",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
+    },
+  },
+};
