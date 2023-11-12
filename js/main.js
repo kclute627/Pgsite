bfc56fa48985caa09f1637813e1edfdc437aeb07
hashirkn <hashir.carbonicit@gmail.com>
2023-11-12 18:09:38 +0500

    updated

--- js/main.js
+++ js/main.js
@@ -17,48 +17,6 @@ function closeMobileNav() {
 navbar.addEventListener("click", openMobileNav);
 closeNav.addEventListener("click", closeMobileNav);
 
-// for counter up animation
-// Function to start the count-up animation
-function startCountUpAnimation() {
-  const amounts = document.querySelectorAll(".amount");
-
-  amounts.forEach((element) => {
-    const amountText = element.textContent; // Get the text content, e.g., '$4.2 Million'
-    const amountValue = parseFloat(amountText.replace(/[^0-9.]/g, "")); // Extract numerical value
-    const parentDiv = element.closest(".record_box"); // Get the parent div
-
-    const totalAmountElement = parentDiv.querySelector(".total_amount");
-    totalAmountElement.textContent = `Total: $0 Million`; // Initial value to start the animation
-
-    // Animate the count-up effect
-    let counter = 0;
-    const interval = setInterval(() => {
-      counter += amountValue / 100; // Increase by a fraction to create the animation effect
-
-      // Ensure the counter doesn't surpass the actual value
-      if (counter >= amountValue) {
-        clearInterval(interval);
-        counter = amountValue;
-      }
-
-      totalAmountElement.textContent = `Total: $${counter.toFixed(1)} Million`;
-    }, 15); // Speed of animation (lower for smoother but heavier performance)
-  });
-}
-
-// Intersection Observer to trigger the animation when the section is in view
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      startCountUpAnimation();
-      observer.unobserve(entry.target);
-    }
-  });
-});
-
-const recordsContent = document.querySelector(".records_content");
-observer.observe(recordsContent);
-
 // Get the button element
 const btn = document.getElementById("button");
 
